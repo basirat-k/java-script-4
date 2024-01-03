514230dfc70426cc97c9c07f8af6ec7bcdba2e77
basirat10 <basiratamusat10@gmail.com>
2024-01-03 15:07:56 +0000

    java script 4

---
+++ main.js
@@ -0,0 +1,11 @@
+// Numbers
+
+//NaN is an acronym for Not a Number
+
+//The Number.isNaN() method determines whether the passed value is NaN AND its type is Number.AND its type is Number.
+
+//The global isNaN() function determies whether a value is NaN or not.
+
+console.log(typeof"Dave");
+console.log(typeof 4);
+console.log( typeof false);
